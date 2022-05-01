b9e1f292a3210554cf9329b9ca69b56151ba3c77
aetheode <nezhivar@protonmail.com>
2022-05-01 03:48:33 -0400

    update electron config to allow for a non-tab option

--- apps/notifications/src/main.js
+++ apps/notifications/src/main.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
 const app = electron.app;
 
+const config = require("../config.json");
+
 app.on('ready', function () {
   const mainWindow = new electron.BrowserWindow({
     webPreferences: {
@@ -9,10 +11,15 @@ app.on('ready', function () {
       webviewTag: true
     }
   });
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  console.log('main line:14', config, config?.tabs?.length);
+  const url = config?.tabs?.length > 0 ? 'file://' + __dirname + '/index.html' : config?.url;
+  mainWindow.loadURL(url);
   mainWindow.on('ready-to-show', function () {
     mainWindow.show();
     mainWindow.focus();
   });
 });
+
+app.on('window-all-closed', () => {
+  app.dock.hide() // for macOS
+})
