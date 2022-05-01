a9dc854e9dc802405edaaf385c0d342192d598c7
hugivar <github.oxes7@simplelogin.com>
2022-05-01 19:44:39 +0000

    update the cli files, add docs, and shared config files

---
+++ packages/tools/cli/commands/setupWakaTime.js
@@ -0,0 +1,21 @@
+import inquirer from 'inquirer';
+import { program } from 'commander';
+import { execSync } from 'child_process';
+
+const setupWakaTime = () => {
+    program
+        .action(() => {
+            execSync(`
+                echo '
+                    hide_file_names = true' | sudo tee -a $HOME/.wakatime.cfg
+            `);
+        })
+        .configureOutput({
+            writeOut: (str) => console.log(`[OUT] ${str}`),
+            writeErr: (str) => process.stdout.write(`[ERR] ${str}`),
+        });
+
+    program.parse();
+}
+
+export default setupWakaTime;
