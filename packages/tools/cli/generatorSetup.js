27006549abc091717f67258de10ba31b8b41a830
hugivar <github.oxes7@simplelogin.com>
2022-05-06 11:32:20 -0400

    update the cli to leverage shared helpers and a dynamic logic split from Commander.js and Inquirer

--- packages/tools/cli/generatorSetup.js
+++ packages/tools/cli/generatorSetup.js
@@ -1,26 +1,51 @@
 #!/usr/bin/env node
 
-import inquirer from 'inquirer';
-
 // Commands
 import createProject from './commands/createProject.js';
+
+import groupByChoices from './helpers/groupByChoices.js';
+
+const choices = [
+    {
+        name: 'Create monorepo project',
+        value: 'create.Project',
+        action: createProject
+    },
+];
 
 export const generatorQuestons = [
     {
         type: 'list',
         name: 'options',
         message: 'What would you like to do',
-        choices: [
-            {
-                name: 'Create monorepo project',
-                value: 'createProject',
-            },
-        ]
+        choices
     },
 ];
 
 export const generatorAnswer = (answers) => {
-    if (answers.options === 'createProject') {
-        createProject();
+    const { action } = choices.find(item => item.value === answers.options);
+
+    if (action) {
+        action();
     }
 };
+
+export const setupGeneratorComannder = (prog) => {
+    const grouped = groupByChoices(choices);
+
+    Object.keys(grouped).map(program => {
+        const item = prog
+            .command(program)
+
+        grouped[program].map(programItem => {
+            item
+                .command(programItem.command)
+                .description(programItem.description)
+                .action(() => {
+                    setupZsh();
+                });
+        });
+    });
+
+    return prog;
+}
