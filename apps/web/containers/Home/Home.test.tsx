8f3fcd86903cd65c6b39a3a546ae425e00ec5859
aetheode <nezhivar@protonmail.com>
2022-05-16 22:29:07 -0400

    feat: implemnet i18n on the homepage

--- apps/web/containers/Home/Home.test.tsx
+++ apps/web/containers/Home/Home.test.tsx
@@ -3,10 +3,23 @@ import { render, screen } from '../../utils/test-utils';
 
 import Home from './Home'
 
-test('Home renders', async () => {
-    render(
+test('Home renders with correct text', async () => {
+  render(
     <Home />
   )
 
-    expect(screen.getByText(/Names are meaningless. Seek knowledge and understanding. Question Everything./i)).toBeTruthy()
+  expect(screen.getByText(/Hey, I'm nezhivar /i)).toBeTruthy()
+
+  expect(screen.getByText(/home.me.subtitle1 /i)).toBeTruthy()
+  expect(screen.getByText(/journal.title/i)).toBeTruthy()
+  expect(screen.getByText(/home.me.visit/i)).toBeTruthy()
+  expect(screen.getByText(/collection.title/i)).toBeTruthy()
+  expect(screen.getByText(/home.me.subtitle2/i)).toBeTruthy()
+
+  expect(screen.getByText(/home.quote.title/i)).toBeTruthy()
+  expect(screen.getByText(/home.quote.desc/i)).toBeTruthy()
+  expect(screen.getByText(/home.quote.author/i)).toBeTruthy()
+
+  expect(screen.getByText(/home.idea.names/i)).toBeTruthy()
+  expect(screen.getByText(/home.idea.anonymity/i)).toBeTruthy()
 })
--- apps/web/containers/Home/Home.tsx
+++ apps/web/containers/Home/Home.tsx
@@ -2,87 +2,84 @@
 import React from "react";
 import Header from "containers/Header";
 import Link from "next/link";
+import { useTranslation } from 'react-i18next';
 
-const Quote = () => (
-  <div className="flex flex-col p-8 mt-4 pb-2 mx-auto max-w-3xl text-grey-darkest dark:text-slate-300">
-    <h3 className="text-center text-3xl">Anonymity</h3>
+const Quote = () => {
+  const { t } = useTranslation();
 
-    <div className="flex items-center justify-center px-5 py-4">
-      <div className="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-800">
-        <div className="w-full mb-6">
-          <div className="text-3xl text-indigo-500 text-left leading-tight h-3">
-            “
+  return (
+    <div className="flex flex-col p-8 mt-4 pb-2 mx-auto max-w-3xl text-grey-darkest dark:text-slate-300">
+      <h3 className="text-center text-3xl">{t('home.quote.title')}</h3>
+      <div className="flex items-center justify-center px-5 py-4">
+        <div className="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-800">
+          <div className="w-full mb-6">
+            <div className="text-3xl text-indigo-500 text-left leading-tight h-3">
+              “
+            </div>
+            <p className="text-sm text-gray-600 text-center px-5">
+              {t('home.quote.desc')}
+            </p>
+            <div className="text-3xl text-indigo-600 text-right leading-tight h-3 -mt-2">
+              ”
+            </div>
           </div>
-          <p className="text-sm text-gray-600 text-center px-5">
-            See that bird? It’s a brown-throated thrush, but in Germany it’s
-            called a Halzenfugel, and in Chinese they call it a Chung Ling and
-            even if you know all those names for it, you still know nothing
-            about the bird. You only know something about people; what they call
-            the bird. Now that thrush sings, and teaches its young to fly, and
-            flies so many miles away during the summer across the country, and
-            nobody knows how it finds its way.
-          </p>
-          <div className="text-3xl text-indigo-600 text-right leading-tight h-3 -mt-2">
-            ”
+          <div className="w-full">
+            <p className="text-md text-indigo-600 font-bold text-center">
+              {t('home.quote.author')}
+            </p>
           </div>
         </div>
-        <div className="w-full">
-          <p className="text-md text-indigo-600 font-bold text-center">
-            Richard Feynman
-          </p>
-        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
-const Home = () => (
-  <>
-    <Header />
-    <div className="flex flex-col p-8 pb-0 mx-auto mt-8 max-w-3xl text-grey-darkest dark:text-slate-300 text-center">
-      <h2 className="text-center text-3xl mb-8">Hey, I'm Nezhivar 👋</h2>
-      <div className="max-w-md">
-        <div className="text-lg leading-8 spaced-y-6">
-          <p className="mb-4">
-            I'm a full-stack engineer, explorer, and lifelong learner. I share
-            thoughts and learnings through my{" "}
-            <Link href="/journal" passHref>
-              <span className="text-black font-bold cursor-pointer dark:text-slate-100">
-                Journal
-              </span>
-            </Link>
-            .
-          </p>
-          <p>
-            Visit my{" "}
-            <Link href="/collection" passHref>
-              <span className="text-black font-bold cursor-pointer dark:text-slate-100">
-                Collection
-              </span>
-            </Link>{" "}
-            page for a glimpse into the various technologies that I am currently
-            using.
-          </p>
+const Home = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col p-8 pb-0 mx-auto mt-8 max-w-3xl text-grey-darkest dark:text-slate-300 text-center">
+        <h2 className="text-center text-3xl mb-8">Hey, I'm Nezhivar 👋</h2>
+        <div className="max-w-md">
+          <div className="text-lg leading-8 spaced-y-6">
+            <p className="mb-4">
+              {t("home.me.subtitle1")}{" "}
+              <Link href="/journal" passHref>
+                <span className="text-black font-bold cursor-pointer dark:text-slate-100">
+                  {t("journal.title")}
+                </span>
+              </Link>
+              .
+            </p>
+            <p>
+              {t("home.me.visit")}{" "}
+              <Link href="/collection" passHref>
+                <span className="text-black font-bold cursor-pointer dark:text-slate-100">
+                  {t('collection.title')}
+                </span>
+              </Link>{" "}
+              {t("home.me.subtitle2")}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <Quote />
-    <div className="flex flex-col mx-auto max-w-3xl text-grey-darkest dark:text-slate-300 text-center">
-      <div className="max-w-md">
-        <div className="text-lg text-grey-darkest leading-8 spaced-y-6">
-          <p className="mb-4">
-            Names are meaningless. Seek knowledge and understanding. Question
-            Everything.
-          </p>
-          <p>
-            Anonymity should be sought after at all costs. Contributions should
-            be judge based on content alone. Not based on the gravitas of
-            author.
-          </p>
+      <Quote />
+      <div className="flex flex-col mx-auto max-w-3xl text-grey-darkest dark:text-slate-300 text-center">
+        <div className="max-w-md">
+          <div className="text-lg text-grey-darkest leading-8 spaced-y-6">
+            <p className="mb-4">
+              {t("home.idea.names")}
+            </p>
+            <p>
+              {t("home.idea.anonymity")}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  )
+};
 
 export default Home;
