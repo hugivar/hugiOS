b49bd8167cc1b6caaaa7ba1b991c4d883bed8887
hugivar <github.oxes7@simplelogin.com>
2022-05-29 21:36:52 -0400

    add the initial starter for solidjs

---
+++ apps/solidjs-starter/src/App.tsx
@@ -0,0 +1,42 @@
+import { createSignal, createEffect } from 'solid-js';
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { Button } from "@nezhos/ui";
+
+function MyComponent(props: any) {
+  return <div>Hello {props.name}</div>
+}
+
+const App: Component = () => {
+  const [first, setFirst] = createSignal("JSON");
+  const [last, setLast] = createSignal("Bourne");
+
+  createEffect(() => console.log(`${first()} ${last()}`));
+  console.log(Button);
+
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+        <Button>Click Me</Button>
+        <button>here</button>
+        <MyComponent name="Nezhivar" />
+      </header>
+    </div>
+  );
+};
+
+export default App;
