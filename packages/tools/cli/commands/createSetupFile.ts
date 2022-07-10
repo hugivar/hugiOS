9da2ae30f20e4db7b448207f56177f0a00b4cd0f
aetheode <nezhivar@protonmail.com>
2022-07-10 00:12:18 -0400

    Implement templating for a basic config file

---
+++ packages/tools/cli/commands/createSetupFile.ts
@@ -0,0 +1,37 @@
+import { program } from 'commander';
+import { exec, execSync } from 'child_process';
+
+const createSetupFile = ({ fileName }: any) => {
+    console.log('createSetupFile line:5', fileName);
+    program
+        .action(() => {
+            execSync(`
+                echo "#!/usr/bin/env node
+                import 'dotenv/config';
+                import inquirer from 'inquirer';
+                import path from 'path';
+                               
+                export const setup = {
+                    name: 'Give me a name',
+                    value: path.parse(__filename).name,
+                };
+                
+                export const choices = [
+                    {
+                        name: 'Add me',
+                        value: '',
+                        action: null
+                    }
+                ];
+                " | tee -a ./cli/config/${fileName}.ts
+            `);
+        })
+        .configureOutput({
+            writeOut: (str) => console.log(`[OUT] ${str}`),
+            writeErr: (str) => process.stdout.write(`[ERR] ${str}`),
+        });
+
+    program.parse();
+}
+
+export default createSetupFile;
---
+++ packages/tools/cli/config/setup.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import 'dotenv/config';
+import inquirer from 'inquirer';
+import path from 'path';
+
+import createSetupFile from 'cli/commands/createSetupFile';
+
+export const setup = {
+    name: 'Config setup',
+    value: path.parse(__filename).name,
+};
+
+export const choices = [
+    {
+        name: 'Create new setup file',
+        value: 'setup.Create',
+        action: () => {
+            inquirer
+                .prompt({
+                    name: 'fileName',
+                    message: 'What is the file name'
+                })
+                .then(createSetupFile);
+        }
+    }
+];
