fe2d1bb11e94a0c869fa1e1b91c33a9ab4c144ce
nezhivar <nezhivar@protonmail.com>
2022-09-11 16:51:59 -0400

    fix: move the export package to be part of the tools pacakge and called from the cli

--- packages/tools/bin/cli.js
+++ packages/tools/bin/cli.js
@@ -1,4 +1,23 @@
 #! /usr/bin/env node
 const { dirname, resolve } = require('path');
-require('ts-node').register();
+const tsConfigPaths = require("tsconfig-paths");
+
+require('ts-node').register({ project: resolve(dirname(require.main.filename), '../tsconfig.json') });
+require('dotenv').config({ path: resolve(dirname(require.main.filename), '../.env') })
+
+const baseUrl = resolve(dirname(require.main.filename), "../");
+
+tsConfigPaths.register({
+    baseUrl,
+    paths: {
+        "cli/commands/*": [
+            "./cli/commands/*"
+        ],
+        "cli/helpers/*": [
+            "./cli/helpers/*"
+        ],
+    },
+});
+
+
 require(resolve(dirname(require.main.filename), '../cli/main.ts'));
--- packages/tools/cli/config/environment.ts
+++ packages/tools/cli/config/environment.ts
@@ -2,11 +2,11 @@
 import path from 'path';
 
 // Commands
-import createRepo from '../commands/createRepo';
-import setupZsh from '../commands/setupZsh';
-import setupWakaTime from '../commands/setupWakaTime';
-import updateFromLocalDotfiles from '../commands/updateFromLocalDotfiles';
-import updateLocalDotfiles from '../commands/updateLocalDotfiles';
+import createRepo from 'cli/commands/createRepo';
+import setupZsh from 'cli/commands/setupZsh';
+import setupWakaTime from 'cli/commands/setupWakaTime';
+import updateFromLocalDotfiles from 'cli/commands/updateFromLocalDotfiles';
+import updateLocalDotfiles from 'cli/commands/updateLocalDotfiles';
 
 export const setup = {
     name: 'Environment setup',
