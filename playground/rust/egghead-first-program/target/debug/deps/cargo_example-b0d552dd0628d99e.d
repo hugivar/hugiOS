2bb79a06b28a38b19c12e569256b49c99f441e57
hugivar <github.oxes7@simplelogin.com>
2023-01-12 22:02:07 -0500

    feat: update rest example wording and add rust program

---
+++ playground/rust/egghead-first-program/target/debug/deps/cargo_example-b0d552dd0628d99e.d
@@ -0,0 +1,5 @@
+/Users/hugofelkel/repos/nezhOS/playground/rust/cargo-example/target/debug/deps/cargo_example-b0d552dd0628d99e.rmeta: src/main.rs
+
+/Users/hugofelkel/repos/nezhOS/playground/rust/cargo-example/target/debug/deps/cargo_example-b0d552dd0628d99e.d: src/main.rs
+
+src/main.rs:
---
+++ playground/rust/egghead-first-program/target/debug/deps/cargo_example-d3de8cae58d3e9a7.d
@@ -0,0 +1,5 @@
+/Users/hugofelkel/repos/nezhOS/playground/rust/cargo-example/target/debug/deps/cargo_example-d3de8cae58d3e9a7.rmeta: src/main.rs
+
+/Users/hugofelkel/repos/nezhOS/playground/rust/cargo-example/target/debug/deps/cargo_example-d3de8cae58d3e9a7.d: src/main.rs
+
+src/main.rs:
