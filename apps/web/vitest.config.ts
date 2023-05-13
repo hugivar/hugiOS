dc58e9cad45be179b77f457b27d7d1b8be2a24b2
hugivar <github.oxes7@simplelogin.com>
2023-05-13 14:58:01 -0400

    Resolve failing test issues

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import postcssNesting from 'postcss-nesting';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   //@ts-ignore
   plugins: [react(), tsconfigPaths()],
+  css: {
+    postcss: {
+      plugins: [
+          tailwindcss,
+          postcssNesting
+        ],
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
