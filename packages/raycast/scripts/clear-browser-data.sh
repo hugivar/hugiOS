17cbf0d304a37f43c6637a7ac2479408064bce46
hugivar <github.oxes7@simplelogin.com>
2024-02-18 15:47:10 -0500

    update clear browser cache to ignore Orion browser

--- packages/raycast/scripts/clear-browser-data.sh
+++ packages/raycast/scripts/clear-browser-data.sh
@@ -4,13 +4,12 @@
 # @raycast.schemaVersion 1
 # @raycast.title Clear Browser Data
 # @raycast.mode fullOutput
-# @raycast.packageName Web Searches
-
 
 # Define the path to Arc's cache and history
 ARC_CACHE_PATH="$HOME/Library/Application Support/Arc/User Data/Default"
 ARC_HISTORY_PATH="$HOME/Library/Application Support/Arc/User Data/Default/History"
 ARC_COOKIES_PATH="$HOME/Library/Application Support/Arc/User Data/Default/Cookies"
+ORION_CACHE_PATH="$HOME/Library/Application Support/Orion/Defaults"
 
 
 # Delete the Arc Browser cache
@@ -37,4 +36,23 @@ if [ -f "$ARC_COOKIES_PATH" ]; then
     echo "Arc browser cookies deleted."
 else
     echo "Arc browser cookies not found."
+fi
+
+# Delete the Orion Browser cache
+if [ -d "$ORION_CACHE_PATH" ]; then
+    rm -rf "$ORION_CACHE_PATH/Local Storage"
+    rm -rf "$ORION_CACHE_PATH/Session Storage"
+    echo "Orion browser cache deleted."
+else
+    echo "Orion browser cache not found."
+fi
+
+# Delete the Orion Browser history
+if [ -d "$ORION_CACHE_PATH" ]; then
+    rm -f "$ORION_CACHE_PATH/history"
+    rm -f "$ORION_CACHE_PATH/history-wal"
+    rm -f "$ORION_CACHE_PATH/history-shm"
+    echo "Orion browser history deleted."
+else
+    echo "Orion browser history not found."
 fi
