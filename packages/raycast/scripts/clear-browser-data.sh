442f70007610d1e36900d855ba4183d17f38bc5a
hugivar <github.oxes7@simplelogin.com>
2024-12-03 20:48:18 -0500

    add notion in fullscreen script

--- packages/raycast/scripts/clear-browser-data.sh
+++ packages/raycast/scripts/clear-browser-data.sh
@@ -5,54 +5,33 @@
 # @raycast.title Clear Browser Data
 # @raycast.mode fullOutput
 
-# Define the path to Arc's cache and history
-ARC_CACHE_PATH="$HOME/Library/Application Support/Arc/User Data/Default"
-ARC_HISTORY_PATH="$HOME/Library/Application Support/Arc/User Data/Default/History"
-ARC_COOKIES_PATH="$HOME/Library/Application Support/Arc/User Data/Default/Cookies"
-ORION_CACHE_PATH="$HOME/Library/Application Support/Orion/Defaults"
+# Define the path to Vivaldi's cache and history
+VIVALDI_CACHE_PATH="$HOME/Library/Application Support/Vivaldi/Default"
+VIVALDI_HISTORY_PATH="$HOME/Library/Application Support/Vivaldi/Default/History"
+VIVALDI_COOKIES_PATH="$HOME/Library/Application Support/Vivaldi/Default/Cookies"
 
-
-# Delete the Arc Browser cache
-if [ -d "$ARC_CACHE_PATH" ]; then
-    rm -rf "$ARC_CACHE_PATH/Local Storage"
-    rm -rf "$ARC_CACHE_PATH/Session Storage"
-    rm -rf "$ARC_CACHE_PATH/Sessions"
-    echo "Arc browser cache deleted."
+# Delete the Vivaldi Browser cache
+if [ -d "$VIVALDI_CACHE_PATH" ]; then
+    rm -rf "$VIVALDI_CACHE_PATH/Local Storage"
+    rm -rf "$VIVALDI_CACHE_PATH/Session Storage"
+    rm -rf "$VIVALDI_CACHE_PATH/Sessions"
+    echo "Vivaldi browser cache deleted."
 else
-    echo "Arc browser cache not found."
+    echo "Vivaldi browser cache not found."
 fi
 
-# Delete the Arc Browser history
-if [ -f "$ARC_HISTORY_PATH" ]; then
-    rm -f "$ARC_HISTORY_PATH"
-    echo "Arc browser history deleted."
+# Delete the Vivaldi Browser history
+if [ -f "$VIVALDI_HISTORY_PATH" ]; then
+    rm -f "$VIVALDI_HISTORY_PATH"
+    echo "Vivaldi browser history deleted."
 else
-    echo "Arc browser history not found."
+    echo "Vivaldi browser history not found."
 fi
 
-# Delete the Arc Browser cookies
-if [ -f "$ARC_COOKIES_PATH" ]; then
-    rm -f "$ARC_COOKIES_PATH"
-    echo "Arc browser cookies deleted."
+# Delete the Vivaldi Browser cookies
+if [ -f "$VIVALDI_COOKIES_PATH" ]; then
+    rm -f "$VIVALDI_COOKIES_PATH"
+    echo "Vivaldi browser cookies deleted."
 else
-    echo "Arc browser cookies not found."
-fi
-
-# Delete the Orion Browser cache
-if [ -d "$ORION_CACHE_PATH" ]; then
-    rm -rf "$ORION_CACHE_PATH/Local Storage"
-    rm -rf "$ORION_CACHE_PATH/Session Storage"
-    echo "Orion browser cache deleted."
-else
-    echo "Orion browser cache not found."
-fi
-
-# Delete the Orion Browser history
-if [ -d "$ORION_CACHE_PATH" ]; then
-    rm -f "$ORION_CACHE_PATH/history"
-    rm -f "$ORION_CACHE_PATH/history-wal"
-    rm -f "$ORION_CACHE_PATH/history-shm"
-    echo "Orion browser history deleted."
-else
-    echo "Orion browser history not found."
+    echo "Vivaldi browser cookies not found."
 fi
